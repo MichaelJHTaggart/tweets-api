781fff1dcabe3c7562ae1bae4e944588ad9892a9
Michael Taggart <mtaggarthawk@gmail.com>
2021-12-14 13:50:26 -0700

    Revert "Merge pull request #6 from MichaelJHTaggart/adam1"
    This reverts commit 220deb52482bf6a73f722cb254152c1daf267793, reversing
    changes made to 9b26fe96036d51567b8776edeca29708ad36a523.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Lemon">
+    <div className="Bob">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
