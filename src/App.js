3816f9662266d6e018af99e9902e3daa10f4aff7
Adam Hunter <hunteradam13@gmail.com>
2021-12-14 13:10:09 -0700

    added info

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (My edit is adding this statement-AH)
         </a>
       </header>
     </div>
